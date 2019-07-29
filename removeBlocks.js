3f5d375dd724454853e15c063b4e0077396f5e2b
Peter-SB <peterboughton11@live.co.uk>
2019-07-29 20:02:31 +0100

    Update removeBlocks.js

--- removeBlocks.js
+++ removeBlocks.js
@@ -2,6 +2,9 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let raf;
 
+let mouseDown = false;
+let counter = 0;
+
 let drawList = [];
 
 //
@@ -53,20 +56,40 @@ class Box extends Shape {
 // Helper Functions
 //
 
+function generateWorld() {
+    for (let i = 0; i < 1000; i++) {
+        var minx = 0;
+        var maxx = ctx.canvas.width - 100;
+        var randomx = Math.floor(Math.random() * (+maxx - +minx)) + +minx;
+        var miny = 0;
+        var maxy = ctx.canvas.height - 100;
+        var randomy = Math.floor(Math.random() * (+maxy - +miny)) + +miny;
+        drawList.push(new Box(randomx, randomy, 100, 100, `rgb(${100 * (i / 1500)},${255 * (i / 1500)},${100 * (i / 1500)}`));
+        console.log(`rgb(50*(${i / 1500}),200*(${i / 1500}),50*(${i / 1500})`);
+    }
+}
+
+
 //
 // Main draw function
 //
+
 function draw() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   drawList.forEach((shape) => {
     shape.draw();
   });
+  counter += 1;
   raf = window.requestAnimationFrame(draw);
 }
 
-ctx.canvas.width  = window.innerWidth-20;
-ctx.canvas.height = window.innerHeight-20;
-drawList.push(new Box(100,100,100,100));
+ctx.canvas.width  = window.innerWidth-40;
+ctx.canvas.height = window.innerHeight-30;
+
+generateWorld();
+
+draw();
+
 
 
 //
@@ -104,7 +127,7 @@ canvas.addEventListener('click', (event) => {
 
   drawList = [...tempDrawList.reverse()]
 
-  if(!itemDeleted) {
+  /*if(!itemDeleted) {
     drawList.push(new Box(
       getMousePos(event).x-50,
       getMousePos(event).y-50,
@@ -112,5 +135,37 @@ canvas.addEventListener('click', (event) => {
       100,
       'rgb(50,200,50'
     ));
-  }
+  }*/
+})
+
+canvas.addEventListener('mousedown', () => {
+    mouseDown = true;
+})
+canvas.addEventListener('mouseup', () => {
+    mouseDown = false;
+})
+
+canvas.addEventListener('mousemove', (event) => {
+    
+    if ((counter >= 3 && mouseDown === true)) {
+        console.log('1');
+        counter = 0
+
+        let itemDeleted = false;
+        const tempDrawList = [...drawList]
+        tempDrawList.reverse().forEach((shape, index) => {
+            if (!itemDeleted) {
+                if (shape.inBoundary(getMousePos(event)) === true) {
+                    shape.delete();
+                    tempDrawList.splice(index, 1);
+                    console.log('shape deleted');
+                    itemDeleted = true;
+                }
+            }
+        });
+
+        drawList = [...tempDrawList.reverse()]
+    }
+
+
 })
