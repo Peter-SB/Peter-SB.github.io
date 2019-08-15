a9fb047c18e34b7b1bd7bb2d8b22b0054f00abf9
Peter-SB-Work <peter.semrau-boughton@cambridge-intelligence.com>
2019-08-15 16:44:23 +0100

    started adding boundarys

--- demos/graph/graph.js
+++ demos/graph/graph.js
@@ -6,6 +6,18 @@ let raf;
 const offset = { x: 150, y: 150 };
 const zoom = 10;
 
+const canvasSize = {
+  width: 300,
+  height: 300,
+};
+
+const boundarys = {
+  xMin: -3,
+  xMax: 3,
+  yMin: -3,
+  yMax: 3,
+};
+
 //
 // Defining objects
 //
@@ -30,7 +42,7 @@ class ComplexNumber {
     this.im = this.im + num2.im;
   }
 
-  magnitude(){
+  magnitude() {
     return Math.sqrt(this.r ** 2 + this.im ** 2);
   }
 }
@@ -49,10 +61,10 @@ function itterate(z, c) {
 // gets the number of itteration needed to make a complex number diverge
 function getItterationsToDivergence(constant, maxItterations) {
   let itteration = 1;
-  let number = new ComplexNumber(0,0);
-  while(number.magnitude()<2 && itteration<=maxItterations) {
+  const number = new ComplexNumber(0, 0);
+  while (number.magnitude() < 2 && itteration <= maxItterations) {
     itterate(number, constant);
-    itteration ++;
+    itteration++;
   }
   console.log(`${number.r} + ${number.im}i`);
   return itteration;
@@ -61,6 +73,20 @@ function getItterationsToDivergence(constant, maxItterations) {
 //
 // Helper Functions
 //
+
+//
+function pixleToAxis() {
+  const xAxis = [];
+  for (let xPixle = 0; xPixle < canvasSize.width; xPixle++) {
+    const axisWidth = boundarys.xMax - boundarys.xMin;
+    xAxis.push((axisWidth / canvasSize.width) * (xPixle + 1) + boundarys.xMin);
+  }
+  return { xAxis };
+}
+
+function axisPointToPixle() {
+
+}
 
 function convertCoords(xRelative, yRelative) {
   const xAbsolute = xRelative + offset.x;
@@ -74,9 +100,16 @@ function convertCoords(xRelative, yRelative) {
 function drawAxis() {
   ctx.fillStyle = 'rgba(100,100,100,1)';
   // draw x axis
-  ctx.fillRect(0, offset.y, ctx.canvas.width, 2);
+  ctx.fillRect(0, offset.y, ctx.canvas.width, 1);
   // draw y axis
-  ctx.fillRect(offset.x, 0, 2, ctx.canvas.height);
+  ctx.fillRect(offset.x, 0, 1, ctx.canvas.height);
+}
+
+function drawPoint(complexPoint, itterations) {
+  const coordinates = convertCoords(complexPoint.r, complexPoint.im);
+  const i = itterations / 10;
+  ctx.fillStyle = `rgb(${255 * (i)},${215 * (i)},${0 * (i)}`;
+  ctx.fillRect(coordinates.x, coordinates.y, 2, 2);
 }
 
 function calculateMandelbrot() {
@@ -84,19 +117,19 @@ function calculateMandelbrot() {
   let y;
   for (x = -4; x < 4; x++) {
     for (y = -4; y < 4; x++) {
-      const itterations = getItterationsToDivergence(new ComplexNumber(x,y),10);
-      drawPoint([x,y], itterations);
+      const itterations = getItterationsToDivergence(new ComplexNumber(x, y), 10);
+      drawPoint([x, y], itterations);
     }
   }
 }
 
-function drawPoint(complexPoint, itterations) {
-  const coordinates = convertCoords(complexPoint.r,complexPoint.im);
-  i = itterations/10;
-  ctx.fillStyle = `rgb(${255 * (i)},${215 * (i)},${0 * (i)}`;
-  ctx.fillRect(coordinates.x, coordinates.y, 2, 2);
+function drawx() {
+  pixleToAxis().xAxis.forEach((xPoint, index) => {
+    ctx.fillStyle = 'rgba(100,100,100,1)';
+    // draw x axis
+    ctx.fillRect(xPoint, xPoint, 1, 1);
+  });
 }
-
 
 //
 // Main draw function
@@ -105,11 +138,11 @@ function drawPoint(complexPoint, itterations) {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawAxis();
-  calculateMandelbrot();
+  // calculateMandelbrot();
 }
 
-ctx.canvas.width = window.innerWidth - 40;
-ctx.canvas.height = window.innerHeight - 30;
+ctx.canvas.width = canvasSize.width;
+ctx.canvas.height = canvasSize.height;
 
 offset.x = ctx.canvas.width / 2;
 offset.y = ctx.canvas.height / 2;
@@ -121,9 +154,9 @@ num1.squared();
 console.log(`${num1.r} + ${num1.im}i`);
 
 console.log(`${num1.magnitude()}`);
-
-getItterationsToDivergence(new ComplexNumber(-1,0),10);
-getItterationsToDivergence(new ComplexNumber(1,0),10);
+pixleToAxis();
+// getItterationsToDivergence(new ComplexNumber(-1, 0), 10);
+// getItterationsToDivergence(new ComplexNumber(1, 0), 10);
 
 draw();
 
